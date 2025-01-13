6cee3613d1de19f0cf57bb7105b3a1f17b6c3572
Alessandra Brondetta <alessandrabrondetta@gmail.com>
2025-01-13 22:11:52 +0100

    introduce documentation for utils module

--- mentevo/utils.py
+++ mentevo/utils.py
@@ -4,12 +4,13 @@ import numpy as np
 def gaussian_g_vector(average, deviation, number_of_agents):
     """
     Create a vector of g values following Gaussian distribution.
+    The average value of the vector is forced to be the average value given as input.
 
     Parameters
     ----------
     average : float
         The average value of the Gaussian distribution.
-        The average should be greater than 0.
+        The average should be greater than 0.0.
     deviation : float
         The standard deviation of the Gaussian distribution.
     number_of_agents : int
@@ -21,17 +22,42 @@ def gaussian_g_vector(average, deviation, number_of_agents):
         A numpy array of size number_of_agents with the g values.           
     """
     assert average > 0, 'average should be greater than 0'
-    assert deviation > 0, 'deviation should be greater than 0'
+    assert deviation >= 0, 'deviation should be non-negative'
 
     g = np.random.normal(average, deviation, number_of_agents)
     g = np.clip(g, 0, None)
+    # used range in Brondetta et al. 2024
+    g[g == 0] = np.random.uniform(0.5, 8.5, size=np.sum(g == 0))
     g = g / (np.mean(g) + 1e-6)
     g = g * average
     return g
 
 
 def uniform_g_vector(average, delta, number_of_agents):
-    assert delta <= average
+    """
+    Create a vector of g values following uniform distribution.
+    The average value of the vector is forced to be the average value given as input.
+    
+    Parameters
+    ----------
+    average : float
+        The average value of the uniform distribution.
+        The average should be greater than 0.0.
+    delta : float
+        The deviation of the uniform distribution.
+    number_of_agents : int
+        The number of agents in the system.
+    
+    Returns
+    -------
+    g : numpy array
+        A numpy array of size number_of_agents with the g values.           
+    """
+
+    assert average > 0, 'average should be greater than 0'
+    assert delta >= 0, 'deviation should be non-negative'
+    assert delta <= average, 'deviation should be less than average'
+
     g = np.random.uniform(average - delta, average + delta, number_of_agents)
     g = g / (np.mean(g) + 1e-6)
     g = g * average
@@ -47,12 +73,13 @@ def build_communication_tensor(number_of_agents, number_of_tasks,
     return A
 
 
-def build_forward_matrix(number_of_agent, number_of_tasks, alpha, beta, gamma, delta, task_graph, communication_graph):
+def build_forward_matrix(number_of_agent, number_of_tasks, alpha, beta, gamma, delta, 
+                         task_graph, communication_graph):
     """
-    todo Ale: do the documentation.
+    Build the forward matrix of the system.
 
-    Tehe forward matrix is a matrix of size (Na * No) x (Na * No)
-    that represents the interaction between agents and tasks.
+    The forward matrix is a matrix of size (Na * No) x (Na * No)
+    that represents the interaction between all agents and tasks.
     The next state of the agents is given by the matrix-vector product
     of the forward matrix and the current state of the agents.
 
@@ -60,7 +87,7 @@ def build_forward_matrix(number_of_agent, number_of_tasks, alpha, beta, gamma, d
     alpha * I + beta * (1 - I)   for the in-diagonal block
     gamma * I + delta * (1 - I)  for the out-diagonal block
 
-    A more advnced formulas that use the graphs is the following:
+    A more advanced formulas that use the graphs is the following:
     alpha * (G_o * I_o) + beta * (G_o - G_o * I_o)   for the in-diagonal block
     gamma * (G_o * I_o) + delta * (G_o - G_o * I_o)  for the out-diagonal block
 
@@ -69,8 +96,44 @@ def build_forward_matrix(number_of_agent, number_of_tasks, alpha, beta, gamma, d
     Furthermore, each block is multiplied by their corresponding scalar value in
     the communication graph.
 
+    Parameters
+    ----------
+    number_of_agent : int
+        Number of agents in the system.
+    number_of_tasks : int
+        Number of tasks the agents have to perform.
+    alpha : float
+        The scalar value that weights the same agent-same task interaction.
+    beta : float
+        The scalar value that weights the same agent-different task interaction.    
+    gamma : float
+        The scalar value that weights the different agent-same task interaction.
+    delta : float
+        The scalar value that weights the different agent-different task interaction.   
+    task_graph : numpy array
+        The graph between tasks. A positive value means that the tasks are positively correlated, 
+        a negative value means that the tasks are negatively correlated. 
+        A null value means that the tasks are not correlated.
+    communication_graph : numpy array
+        The graph between agents. A positive value means that the agents have a positive interaction, 
+        a negative value means that the agents have a negative interaction. 
+        A null value means that the agents can not communicate.
+
+    Returns
+    -------
+    F : numpy array
+        The forward matrix of the system.
     """
-    # todo: check the shapes of graphs!!
+
+    assert number_of_agent > 0, 'number_of_agent should be greater than 0'
+    assert number_of_tasks > 0, 'number_of_tasks should be greater than 0'
+    assert alpha >= 0, 'alpha should be non-negative'
+    assert beta >= 0, 'beta should be non-negative'
+    assert gamma >= 0, 'gamma should be non-negative'
+    assert delta >= 0, 'delta should be non-negative'
+    assert task_graph.shape == (number_of_tasks, number_of_tasks), 'task_graph should be of size No x No'
+    assert communication_graph.shape == (number_of_agent, number_of_agent), 'communication_graph should be of size Na x Na' 
+    
     # diagonal blocks of the forward matrix
     diagonal_block = alpha * (task_graph * np.eye(number_of_tasks)
                               ) + beta * (task_graph - task_graph * np.eye(number_of_tasks))
@@ -89,10 +152,51 @@ def build_forward_matrix(number_of_agent, number_of_tasks, alpha, beta, gamma, d
 def build_cue_vector(number_of_agents, number_of_tasks, n_informed,
                      n_switches, total_time, reversed=False):
     """
-    todo the documentation
+    Build the cue vector for the experiment.
+    The cue vector is a vector of size total_time x (Na * No) that informs the agents about the tasks 
+    they should perform at each time step. The cue vector has the following characteristics.
+    - The shape is a step function with n_switches regular steps. Every step has the same length given 
+    by total_time // n_switches.
+    - The first step is a vector of 1 for the first task and -1 for all the other tasks,
+    meaning that Task 1 is prioratized.
+    - The vector is then rotated by one position for each step, meaning that now Task 2 is prioratized.
+    etc.
+    - When reversed is True, the cue vector is reversed.
+    - The informed agents are the first n_informed agents in the system and are the only ones 
+    receiving the task cue. The other agents receive a vector of 0.
+
+    Parameters
+    ----------
+    number_of_agents : int
+        Number of agents in the system.
+    number_of_tasks : int
+        Number of tasks the agents have to perform.
+    n_informed : int
+        Number of agents that are informed about the tasks.
+    n_switches : int
+        Number of switches in the cue vector.
+    total_time : int
+        Total time of the experiment.
+    reversed : bool, optional
+        This options is used to reverse the cue vector. If True, the cue vector is reversed.
+        Default is False.  
+
+    Returns
+    -------
+    cue_vector : numpy array
+        The cue vector of the experiment.
     """
-    n_switches = int(n_switches)  # todo: ale see if we should assert here
-    n_informed = int(n_informed)  # todo: ale see if we should assert here
+
+    assert number_of_agents > 0, 'number_of_agents should be greater than 0'
+    assert number_of_tasks > 0, 'number_of_tasks should be greater than 0'
+    assert n_informed >= 0, 'n_informed should be non-negative'
+    assert n_informed <= number_of_agents, 'n_informed should be less or equal to number_of_agents'
+    assert n_switches >= 0, 'n_switches should be non-negative'
+    assert n_switches <= total_time, 'n_switches should be less than total_time'
+    assert total_time > 0, 'total_time should be greater than 0'
+
+    n_switches = int(n_switches)  
+    n_informed = int(n_informed)  
     switch_len = total_time // n_switches
 
     cue_vector = np.zeros((total_time, number_of_agents * number_of_tasks))
@@ -101,18 +205,88 @@ def build_cue_vector(number_of_agents, number_of_tasks, n_informed,
     val = -1.0 * np.ones(number_of_tasks)
     val[0] = 1.0
     val = np.array(list(val) * number_of_agents)
-    # val = np.ones((number_of_agents * number_of_tasks))
-    # for i in range(number_of_tasks):
-    #    val[i * number_of_tasks + 1: (i + 1) * number_of_tasks] = -1
-    # if number informed is less than number of agents, we need to set the
-    # remaining agents to 0
+    # if number informed is less than number of agents, we need to set the remaining agents to 0
     if n_informed < number_of_agents:
         val[n_informed * number_of_tasks:] = 0
 
     for i in range(int(n_switches)):
         cue_vector[i*switch_len:(i+1)*switch_len, :] = val
-        # val = np.roll(val, 1)
-        # val = np.roll(val, number_of_tasks)
+        val = val.reshape((number_of_agents, number_of_tasks))
+        val = np.roll(val, 1, axis=1)
+        val = val.flatten()
+
+    if reversed:
+        cue_vector = -1.0 * cue_vector
+
+    return cue_vector
+
+
+def build_cue_vector_initial_time(number_of_agents, number_of_tasks, n_informed,
+                     n_switches, total_time, initial_steps = 0, reversed=False):
+    """
+    Build the cue vector for the experiment with initial_steps indicates the
+    first time steps where no tasks are prioritized.
+    The cue vector is a vector of size total_time x (Na * No) that informs the agents about the tasks 
+    they should perform at each time step. The cue vector has the following characteristics:
+    - The first initial_steps steps are vectors of zero, meaning no tasks are prioritized.
+    - After the cue vector behaves like the original function, switching tasks at regular intervals.
+    - When reversed is True, the cue vector is reversed.
+    - The informed agents are the first n_informed agents in the system and are the only ones 
+      receiving the task cue. The other agents receive a vector of 0.
+
+    Parameters
+    ----------
+    number_of_agents : int
+        Number of agents in the system.
+    number_of_tasks : int
+        Number of tasks the agents have to perform.
+    n_informed : int
+        Number of agents that are informed about the tasks.
+    n_switches : int
+        Number of switches in the cue vector.
+    total_time : int
+        Total time of the experiment.
+    initial_steps : int
+        Number of initial time steps where no task is prioritized.
+    reversed : bool, optional
+        This options is used to reverse the cue vector. If True, the cue vector is reversed.
+        Default is False.  
+
+    Returns
+    -------
+    cue_vector : numpy array
+        The cue vector of the experiment.
+    """
+
+    assert number_of_agents > 0, 'number_of_agents should be greater than 0'
+    assert number_of_tasks > 0, 'number_of_tasks should be greater than 0'
+    assert n_informed >= 0, 'n_informed should be non-negative'
+    assert n_informed <= number_of_agents, 'n_informed should be less or equal to number_of_agents'
+    assert n_switches >= 0, 'n_switches should be non-negative'
+    assert n_switches <= total_time - initial_steps, 'n_switches should be less than or equal to total_time - initial_steps'
+    assert total_time > 0, 'total_time should be greater than 0'
+    assert initial_steps >= 0, 'initial_steps should be non-negative'
+
+    n_switches = int(n_switches)  
+    n_informed = int(n_informed)  
+    switch_len = (total_time - initial_steps) // n_switches  # Adjust the switching length after initial steps
+
+    cue_vector = np.zeros((total_time, number_of_agents * number_of_tasks))
+
+    # first value is 1 for the first task and -1 for all the other tasks
+    val = -1.0 * np.ones(number_of_tasks)
+    val[0] = 1.0
+    val = np.array(list(val) * number_of_agents)
+    # if number informed is less than number of agents, we need to set the remaining agents to 0
+    if n_informed < number_of_agents:
+        val[n_informed * number_of_tasks:] = 0
+
+    # Fill the first time steps with zeros (no tasks prioritized)
+    cue_vector[:initial_steps, :] = 0
+
+    # Now start the task switching after the initial steps
+    for i in range(n_switches):
+        cue_vector[initial_steps + i*switch_len:initial_steps + (i+1)*switch_len, :] = val
         val = val.reshape((number_of_agents, number_of_tasks))
         val = np.roll(val, 1, axis=1)
         val = val.flatten()
--- tests/test_cue_vectors.py
+++ tests/test_cue_vectors.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mentevo.utils import build_cue_vector
+from mentevo.utils import build_cue_vector_initial_time
 
 from .utils import epsilon_equal
 
@@ -9,7 +10,7 @@ def test_build_cue_vector():
     nt = 2
     n_switches = 3
     total_time = 6
-    v = build_cue_vector(na, nt, na, n_switches, total_time)
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time)
 
     assert v.shape == (total_time, na * nt)
     assert epsilon_equal(v, np.array([
@@ -30,7 +31,7 @@ def test_build_cue_vector_reversed():
     nt = 2
     n_switches = 3
     total_time = 6
-    v = build_cue_vector(na, nt, na, n_switches, total_time, reversed=True)
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, reversed=True)
 
     assert v.shape == (total_time, na * nt)
     assert epsilon_equal(v, np.array([
@@ -51,7 +52,7 @@ def test_build_cue_vector_3_tasks():
     nt = 3
     n_switches = 3
     total_time = 6
-    v = build_cue_vector(na, nt, na, n_switches, total_time)
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time)
 
     assert v.shape == (total_time, na * nt)
     assert epsilon_equal(v, np.array([
@@ -74,7 +75,7 @@ def test_build_cue_vector_half_agent():
     n_switches = 3
     total_time = 6
 
-    v = build_cue_vector(na, nt, nb_informed, n_switches, total_time)
+    v = build_cue_vector_initial_time(na, nt, nb_informed, n_switches, total_time)
 
     assert v.shape == (total_time, na * nt)
     assert epsilon_equal(v, np.array([
@@ -97,7 +98,7 @@ def test_zero_agent():
     n_switches = 3
     total_time = 6
 
-    v = build_cue_vector(na, nt, nb_informed, n_switches, total_time)
+    v = build_cue_vector_initial_time(na, nt, nb_informed, n_switches, total_time)
 
     assert v.shape == (total_time, na * nt)
     assert epsilon_equal(v, np.zeros((total_time, na * nt)))
@@ -109,7 +110,7 @@ def test_scaling():
     n_switches = 4
     total_time = 1_000
 
-    v = build_cue_vector(na, nt, na, n_switches, total_time)
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time)
 
     assert v.shape == (total_time, na * nt)
 
@@ -143,3 +144,199 @@ def test_scaling():
 
     # check the second one activate task 2 only
     assert np.all(v[250] == np.array([-1, 1] * na))
+
+
+#------------------------------------------------------------
+
+def test_build_cue_vector_t0_zero():
+    na = 2
+    nt = 2
+    n_switches = 3
+    total_time = 6
+    t0 = 0
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert epsilon_equal(v, np.array([
+        # switch 1 (task 1)
+        [1, -1, 1, -1],
+        [1, -1, 1, -1],
+        # switch 2 (task 2)
+        [-1, 1, -1, 1],
+        [-1, 1, -1, 1],
+        # switch 3 (task 1)
+        [1, -1, 1, -1],
+        [1, -1, 1, -1],
+    ]))
+
+def test_build_cue_vector_t0():
+    na = 2
+    nt = 2
+    n_switches = 3
+    total_time = 8
+    t0 = 2
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 6
+    assert epsilon_equal(v, np.array([
+        # switch 0 
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        # switch 1 (task 1)
+        [1, -1, 1, -1],
+        [1, -1, 1, -1],
+        # switch 2 (task 2)
+        [-1, 1, -1, 1],
+        [-1, 1, -1, 1],
+        # switch 3 (task 1)
+        [1, -1, 1, -1],
+        [1, -1, 1, -1],
+    ]))
+
+
+def test_build_cue_vector_reversed_t0():
+    na = 2
+    nt = 2
+    n_switches = 3
+    total_time = 8
+    t0 = 2
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, t0, reversed=True)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 6
+    assert epsilon_equal(v, np.array([
+        # switch 0 
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        # switch 1 (task 2)
+        [-1, 1, -1, 1],
+        [-1, 1, -1, 1],
+        # switch 2 (task 1)
+        [1, -1, 1, -1],
+        [1, -1, 1, -1],
+        # switch 3 (task 2)
+        [-1, 1, -1, 1],
+        [-1, 1, -1, 1],
+    ]))
+
+
+def test_build_cue_vector_3_tasks_t0():
+    na = 2
+    nt = 3
+    n_switches = 3
+    total_time = 8
+    t0 = 2
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 6
+    assert epsilon_equal(v, np.array([
+        # switch 0 
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        # switch 1 (task 1)
+        [1, -1, -1, 1, -1, -1],
+        [1, -1, -1, 1, -1, -1],
+        # switch 2 (task 2)
+        [-1, 1, -1, -1, 1, -1],
+        [-1, 1, -1, -1, 1, -1],
+        # switch 3 (task 3)
+        [-1, -1, 1, -1, -1, 1],
+        [-1, -1, 1, -1, -1, 1],
+    ]))
+
+
+def test_build_cue_vector_half_agent_t0():
+    na = 2
+    nt = 2
+    nb_informed = 1
+    n_switches = 3
+    total_time = 8
+    t0 = 2
+
+    v = build_cue_vector_initial_time(na, nt, nb_informed, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 6
+    assert epsilon_equal(v, np.array([
+        # switch 0 
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        # switch 1 (task 1)
+        [1, -1, 0, 0],
+        [1, -1, 0, 0],
+        # switch 2 (task 2)
+        [-1, 1, 0, 0],
+        [-1, 1, 0, 0],
+        # switch 3 (task 1)
+        [1, -1, 0, 0],
+        [1, -1, 0, 0],
+    ]))
+
+
+def test_zero_agent_t0():
+    na = 2
+    nt = 2
+    nb_informed = 0
+    n_switches = 3
+    total_time = 8
+    t0 = 2
+
+    v = build_cue_vector_initial_time(na, nt, nb_informed, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 6
+    assert epsilon_equal(v, np.zeros((total_time, na * nt)))
+
+
+def test_scaling_t0():
+    na = 100
+    nt = 2
+    n_switches = 4
+    total_time = 1_200
+    t0 = 200
+
+    v = build_cue_vector_initial_time(na, nt, na, n_switches, total_time, t0)
+
+    assert v.shape == (total_time, na * nt)
+    assert total_time - t0 == 1_000
+
+    # check that the zero switch is the same for all agents
+    for i in range(199):
+        assert epsilon_equal(v[0], v[i])
+
+    # check that the first switch is the same for all agents
+    for i in range(200, 449):
+        assert epsilon_equal(v[200], v[i])
+
+    # check that the second switch is the same for all agents
+    for i in range(450, 699):
+        assert epsilon_equal(v[450], v[i])
+
+    # check that the third switch is the same for all agents
+    for i in range(700, 949):
+        assert epsilon_equal(v[700], v[i])
+
+    # check that the fourth switch is the same for all agents
+    for i in range(950, 1199):
+        assert epsilon_equal(v[950], v[i])
+
+    # now check that first and second switch are different
+    assert not epsilon_equal(v[200], v[450])
+
+    # now check that the first and third switch are the same
+    assert epsilon_equal(v[200], v[700])
+
+    # now check that the first and fourth switch are the same
+    assert epsilon_equal(v[450], v[950])
+
+    # check the first one activate task 1 only
+    assert np.all(v[200] == np.array([1, -1] * na))
+
+    # check the second one activate task 2 only
+    assert np.all(v[450] == np.array([-1, 1] * na))
+
+    # check the zero one activate no task 
+    assert np.all(v[0] == np.array([0, 0] * na))
+
